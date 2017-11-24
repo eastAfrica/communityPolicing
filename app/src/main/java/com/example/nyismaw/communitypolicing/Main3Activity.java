c8efd2446a2b668a3402f5bac8ff260ee107690e
John Munyi <wachirawamunyi@gmail.com>
2017-11-25 00:31:43 +0200

    Added tabbed slide activities

---
+++ app/src/main/java/com/example/nyismaw/communitypolicing/Main3Activity.java
@@ -0,0 +1,32 @@
+package com.example.nyismaw.communitypolicing;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+
+public class Main3Activity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private SectionsPageAdapter mSectionPageAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "TAB1");
+        adapter.addFragment(new Tab2Fragment(), "TAB2");
+        viewPager.setAdapter(adapter);
+    }
+}
